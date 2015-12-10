898f39ba1e27444ed58cd496b6c677efaa570af3
Scott King <kingsc850@gmail.com>
2015-12-10 15:28:56 -0500

    babel rules in tag.js

--- app/www/js/components/Tag.js
+++ app/www/js/components/Tag.js
@@ -9,7 +9,7 @@ class Tag extends Component {
     const {tag, selected, onChange} = this.props;
     let button;
     let c = color({r: tag.color[0], g: tag.color[1], b: tag.color[2]}).hexString();
-    let style = { margin: 5 };
+    let style = {margin: 5};
     if (selected) {
       button = (<RaisedButton style={style} label={tag.label} primary={true} onClick={onChange} backgroundColor={c} />);
     } else {
